de19862ffebde04dc3eaa15719fcabbc0738a404
Quevin Bambasi <quevinbambasi@pm.me>
2023-07-11 20:39:28 -0400

    Add "__str__" to Topic model

--- base/models.py
+++ base/models.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import User
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
