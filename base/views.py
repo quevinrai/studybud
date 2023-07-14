02b6950e310b8875180784019de80e3d8f38c3bc
Quevin Bambasi <quevinbambasi@pm.me>
2023-07-14 15:37:13 -0400

    Add condition • Check if request method is 'POST'

--- base/views.py
+++ base/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            pass
+
     context = {}
     return render(request, 'base/login_register.html', context)
 
