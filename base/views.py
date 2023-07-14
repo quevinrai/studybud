03c188ff5b410efd8e2c3c91377cbb247335d978
Quevin Bambasi <quevinbambasi@pm.me>
2023-07-14 17:42:40 -0400

    Replace 'login_required' to 'login_url'

--- base/views.py
+++ base/views.py
@@ -52,7 +52,7 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
-@login_required(login_required='/login')
+@login_required(login_url='/login')
 def createRoom(request):
     form = RoomForm()
 
