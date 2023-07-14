81c5d73d0aac2c57519051d6e04b465e5cd0a43d
Quevin Bambasi <quevinbambasi@pm.me>
2023-07-13 20:31:58 -0400

    Add 'create-room' path

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('room/<str:pk>/', views.room, name = 'room'),
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('create-room/', views.createRoom, name='create-room'),
 ]
