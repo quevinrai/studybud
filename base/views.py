32c4d73fa39ea9965aa91790cd09bc8332454b5a
Quevin Bambasi <quevinbambasi@pm.me>
2023-07-14 15:57:27 -0400

    Import from django.contrib.auth • Import authenticate, login, logout • Update loginPage() and use authenticate & login

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -15,6 +16,14 @@ def loginPage(request):
             user = User.objects.get(username=username)
         except:
             messages.error(request, 'User does not exist.')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exist.')
 
     context = {}
     return render(request, 'base/login_register.html', context)
