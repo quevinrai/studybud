3dc438e693bf0a58272e9501494e541f8a0935a2
Quevin Bambasi <quevinbambasi@pm.me>
2023-07-14 11:58:26 -0400

    Remove whitespace

--- base/views.py
+++ base/views.py
@@ -12,7 +12,7 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = Room.objects.get(id = pk)
+    room = Room.objects.get(id=pk)
 
     context = {'room': room}
     return render(request, 'base/room.html', context)
