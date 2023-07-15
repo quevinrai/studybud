c393b17d6321ffde8b44e8f10edb5b34f55d9f56
Quevin Bambasi <quevinbambasi@pm.me>
2023-07-15 13:27:08 -0400

    Add 'messages' property in room() • Push 'messages' property to context

--- base/views.py
+++ base/views.py
@@ -74,8 +74,9 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+    messages = room.message_set.all()
 
-    context = {'room': room}
+    context = {'room': room, 'messages': messages}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='/login')
